4c73da816e1ac864f212ac11f1f4fb4362a16f37
Jayantsinghsaini <146630172+Jayantsinghsaini@users.noreply.github.com>
2024-09-20 13:54:23 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,45 @@
+/***
+ * this is a Constant class which contains hard coded value.
+ * Name - Jayant Singh
+ * Date - 19/19/24
+ */
+public class Constant {
+        public static final String ENTER_TEAM = "Enter team names (type 'done' when finished):";
+        public static final String SELECT_TEAM = "Select a team to add players (type 'done' to stop):";
+        public static final String INVALID_TEAM = "Invalid team name.";
+        public static final String YES_NO = "Do you want to continue adding players to other teams? (yes/no)";
+        public static final String SELECT_TWO_TEAM = "Select two teams for the match:";
+        public static final String SELECT_FIRST_TEAM = "Select the first team:";
+        public static final String SELECT_SECOND_TEAM = "Select the second team:";
+        public static final String DUPLICATE_TEAM = "Invalid or duplicate team.";
+        public static final String INVALID_PLAYER = "Invalid player. ";
+        public static final String ENTER_OVER = "Enter over to be played";
+        public static final String EXCEPTION_INPUT = "Exception, invalid input ";
+        public static final String TIE = " --------- It's a tie! --------- ";
+        public static final String ENTER_RUN = "Enter runs (0, 1, 2, 3, 4, 6) or type 'out','wide', 'noball':";
+        public static final String ALL_OUT = "All team is out!";
+        public static final String WIDE_EXTRA = "enter extra run for wide";
+        public static final String INVALID_INPUT = "invalid input";
+        public static final String NOBALL_EXTRA = "enter extra run for noball";
+        public static final String OVER_COMPLETE = "Over completed. Select a new bowler:";
+        public static final String ENTER_ATLEAST_11 = "You must enter at least 11 players for the team.";
+        public static final String ENTER_PLAYER = "Enter players for team ";
+        public static final String DONE_STOP = " (type 'done' to stop):";
+        public static final String SELECT_11_PLAYERS = "Select 11 players from following for team ";
+        public static final String BAT_FIRST = "Which team will bat first? ";
+        public static final String FINISH_SCORE= " finished with a score of ";
+        public static final String SELECT_ONSTRICK= "Select on-strike batsman for team ";
+        public static final String SELECT_NONSTRICK= "Select non-strike batsman for team ";
+        public static final String SELECT_BOWLER= "Select bowler for team ";
+        public static final String IS_OUT = " is out. Select a new batsman:";
+        public static final String WIDE_SCORE = "Wide ball! Score: ";
+        public static final String NOBALL_SCORE = "No ball! Score: ";
+        public static final String WICKET_REMAIN = " Team wins! with remaining wicket of ";
+
+
+
+}
+
+
+
+
